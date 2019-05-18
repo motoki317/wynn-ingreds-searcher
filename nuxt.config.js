95b6cb8c14cd7e9f21d4dc0c1fa7a7bb5fe15d7c
motoki317 <motoki317@gmail.com>
2019-05-18 22:20:18 +0900

    proxy???

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,13 +2,9 @@ module.exports = {
   /*
    ** Nuxt.js modules
    */
-  modules: ["@nuxtjs/proxy"],
+  modules: ["@nuxtjs/axios", "@nuxtjs/proxy"],
+  axios: {},
   proxy: {
-    "^/api": {
-      target: "https://api.wynncraft.com/v2/ingredient",
-      pathRewrite: {
-        "^/api/": "/"
-      }
-    }
+    "/api": "https://api.wynncraft.com/v2/ingredient"
   }
 };
